6cd3613b7e98e713c48302c77e2fdf8b2aace193
Odixi <vz4ev4r@gmail.com>
2017-02-22 18:51:29 +0200

    3

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     console.warn('document is ready.');
     $.get('/employee/getEmployees', function(data, status) {
         console.log(data);
+        renderTable(data);
     })
-    console.warn('this line is after ajax call.');
+    renderTable = function(data){
+      for(var i=0; i<data.length; i++) {
+          $('#employee-table').find('tbody')
+          .append('<tr><td>' + data[i].first_name + '</td><td>' + data[i].last_name + '</td><td>' + data[i].email + '</td></tr>');
+    }
+  }
 })
